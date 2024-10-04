0b33104b83e24068405adbe23d2f36b76edb0e35
paulaminguez14 <dominguez.bernabe.paula@iescamas.es>
2024-10-04 08:44:35 +0200

    añadido el 7

--- ejercicio7/multiplicar.php
+++ ejercicio7/multiplicar.php
@@ -3,15 +3,25 @@
     $numero = $_POST['numero'];
     $multiplicacion = 0;
 
+    // if (isset($_GET["numero"])) { para comprobar si no viene vacio }
+    // if (is_numeric($_GET["numero"])) { para comprobar si es un numero }
+
     if ($numero >= 1 && $numero <= 10) {
         echo "Tabla de multiplicar del $numero: ";
         echo "</br>";
 
+        echo "<table>";
         for ($i = 1; $i <= 10; $i++) {
             $multiplicacion = $i * $numero;
-            echo $numero . " x " . $i . " = " . $multiplicacion;
-            echo "</br>";
+            echo "<tr>";
+            echo "<td>$numero</td>
+            <td>x</td>
+            <td>$i</td>
+            <td>=</td>
+            <td>$multiplicacion</td>";
+            echo "</tr>";
         } 
+        echo "</table>";
     } else {
         echo "El número debe ser entre 1 y 10.";
     }
